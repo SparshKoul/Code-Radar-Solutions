12186e3cac1de217dd77b560355daac3a908cf71
Sparsh Koul <sparshkoul6@gmail.com>
2025-02-06 17:10:41 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -1,30 +1,34 @@
-// #include <stdio.h>
-
-// char* welcome() {
-//     return "Welcome to Code Radar!";
-// }
-
-// int main() {
-//      int num1, num2;
-//     scanf("%d %d", &num1, &num2);
-//     if(num1*num2 <0){
-//         printf("True");
-//     }else{
-//         printf("False");
-//     }
-//     return 0;
-// }
 #include <stdio.h>
 
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
 int main() {
-    int num1, num2;
+     int num1, num2;
     scanf("%d %d", &num1, &num2);
-
-    if ((num1 ^ num2) < 0) {  // Checks for opposite signs using XOR
-        printf("True\n");
-    } else {
-        printf("False\n");
+    if(num1*num2 <0){
+        printf("True");
+    }else{
+        printf("False");
     }
-
     return 0;
 }
+
+
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int num1, num2;
+//     scanf("%d %d", &num1, &num2);
+
+//     if ((num1 ^ num2) < 0) {  // Checks for opposite signs using XOR
+//         printf("True\n");
+//     } else {
+//         printf("False\n");
+//     }
+
+//     return 0;
+// }
