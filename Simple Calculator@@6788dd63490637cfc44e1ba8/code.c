ac976a11f4cab14e03f2b2d773262a8983975345
Sparsh Koul <sparshkoul6@gmail.com>
2025-02-18 04:08:03 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -8,10 +8,10 @@ int main() {
 
     switch (operator) {
         case '+':
-            printf("Result: %d\n", num1 + num2);
+            printf("%d\n", num1 + num2);
             break;
         case '-':
-            printf("Result: %d\n", num1 - num2);
+            printf("%d\n", num1 - num2);
             break;
         case '*':
             printf("%d\n", num1 * num2);
